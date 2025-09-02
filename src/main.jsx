b37ca3f3934c7b2e722a8d1fc5ded103d2b08453
Seilani Fontes <seilani.sousa2@gmail.com>
2025-09-02 17:58:39 -0300

    atualizaca ads

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 
-const GA_MEASUREMENT_ID = 'G-Y0HM6WTX34'  // seu ID do GA4
-
-function loadGAScript() {
-  const script = document.createElement('script')
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
-  script.async = true
-  document.head.appendChild(script)
-
-  window.dataLayer = window.dataLayer || []
-  function gtag(){window.dataLayer.push(arguments)}
-  gtag('js', new Date())
-  gtag('config', GA_MEASUREMENT_ID)
-}
-
-loadGAScript()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
